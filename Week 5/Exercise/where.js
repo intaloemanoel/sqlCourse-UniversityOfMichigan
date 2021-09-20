67c560e09e34c65a5625cbc5b8cbf21ebb286bdd
Ítalo Emanoel <italoemanoel@intalo.net>
2021-09-20 01:12:56 -0300

    ✅ Week 5 done

---
+++ Week 5/Exercise/where.js
@@ -0,0 +1,16 @@
+myData = [
+[-22.8626877,-43.2258954, 'Cidade Universitária, Rio de Janeiro - State of Rio de Janeiro, Brazil'],
+[50.0676785,19.9070824, 'Dormitory no. 14 "Kapitol" of AGH University of Science and Technology in Cracov, Poland, Witolda Budryka 2, 30-072 Kraków, Poland'],
+[52.2296756,21.0122287, 'Warsaw, Poland'],
+[30.0068972,31.5005988, 'American University Housing District, Cairo Governorate, Egypt'],
+[33.4232051,-111.8879509, 'State Ave, Tempe, AZ 85281, USA'],
+[33.9094658,-83.4604372, 'Information Technology Services, 2500 Daniels Bridge Rd, Athens, GA 30606, USA'],
+[28.3580163,75.5887989, 'BITS, Pilani, Rajasthan 333031, India'],
+[43.1294077,-77.5479304, 'Babcock Dr, Rochester, NY 14610, USA'],
+[25.2685304,82.9904737, 'Banaras Hindu University Campus, Varanasi, Uttar Pradesh, India'],
+[12.9426914,77.5053145, 'Bangalore University Rd, Bengaluru, Karnataka 560056, India'],
+[-22.7692349,-43.6850841, 'Km 07,Zona Rural - BR-465, Seropédica - RJ, 23890-000, Brazil'],
+[31.5488923,-97.1130573, '1311 S 5th St, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, Bei Tai Ping Zhuang, Haidian Qu, Beijing Shi, China, 100875'],
+[53.8938988,27.5460609, 'prasp. Niezaliežnasci 4, Minsk, Belarus']
+];
